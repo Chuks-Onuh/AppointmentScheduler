9d9c0cf80e95dcdd75f7dc8528df965d9e0b01e5
Onuh Chukwuma. M <onuhchukwuma12@gmail.com>
2021-07-31 13:27:55 +0100

    Login view

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using AppointmentScheduler.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduler.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _ctx;
+
+        public AccountController(AppDbContext context)
+        {
+            _ctx = context;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
