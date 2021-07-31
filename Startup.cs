66874ff256939ca3cef9d12c35f8de4c84493a4d
Onuh Chukwuma. M <onuhchukwuma12@gmail.com>
2021-07-31 14:37:53 +0100

    Login partial view

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using AppointmentScheduler.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,6 +25,8 @@ namespace AppointmentScheduling
 
             services.AddDbContext<AppDbContext>(options =>
            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
